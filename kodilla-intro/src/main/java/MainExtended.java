122c4ce610c09b59eb0227b096a689176a3ab3fb
kmizera <karina.mizera@gmail.com>
2025-02-10 00:48:36 +0100

    adding MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String city = "Kraków";
+        String postalCode = "30-016";
+        int houseNumber = 22;
+        int flatNumber = 15;
+        char parkingZone = 'A';
+        boolean isCityCenter = true;
+        System.out.println(city);
+        System.out.println(postalCode);
+        System.out.println(houseNumber);
+        System.out.println(flatNumber);
+        System.out.println(parkingZone);
+        System.out.println(isCityCenter);
+
+    }
+}
