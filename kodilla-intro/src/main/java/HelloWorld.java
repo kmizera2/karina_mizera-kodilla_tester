5f4c68db0d11e6c20e0cc6e1ef14016cf729b41d
kmizera <karina.mizera@gmail.com>
2025-02-10 00:15:41 +0100

    Zadanie: Hello World!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Karina");
+        System.out.println("47");
+        System.out.println("M");// [3]
+    }
+}
