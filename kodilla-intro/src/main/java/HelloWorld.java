8e9b92ee4dddd5fc33a87043e2d850eef6190974
kmizera <karina.mizera@gmail.com>
2025-02-10 00:22:15 +0100

    removing unnecessary comments

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
-    public static void main(String[] args) {         // [2]
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Karina");
         System.out.println("47");
-        System.out.println("M");// [3]
+        System.out.println("M");
     }
 }
