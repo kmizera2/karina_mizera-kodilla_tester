932bf0203396635818b1425cdf5ae4bb8dd7cc08
kmizera <karina.mizera@gmail.com>
2025-02-10 03:30:30 +0100

    cleaning code LeapYear

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -4,7 +4,7 @@ public class LeapYear {
     public static boolean leapYear (int year){
             if (year%4 != 0) {
                 return false;
-            } else if (year%100!= 0) {
+            } else if (year%100 != 0) {
                 return false;
             } else if (year%400 != 0) {
                 return false;
