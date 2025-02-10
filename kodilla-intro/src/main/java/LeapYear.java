51eb672f2fe17c02421a916fca93c297a4b4b851
kmizera <karina.mizera@gmail.com>
2025-02-10 02:35:23 +0100

    checking leap year

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class LeapYear {
+    public static boolean leapYear (int year){
+            if (year%4 != 0) {
+                return false;
+            } else if (year%100!= 0) {
+                return false;
+            } else if (year%400 != 0) {
+                return false;
+            } else return true;
+        }
+    }
+
+
+
+
