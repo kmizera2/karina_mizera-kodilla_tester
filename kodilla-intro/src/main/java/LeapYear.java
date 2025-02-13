10cbaee3d15d670150a96ce079e370d91469cf37
kmizera <karina.mizera@gmail.com>
2025-02-13 11:20:50 +0100

    fix LeapYear

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 public class LeapYear {
     public static boolean leapYear (int year){
-            if (year%4 != 0) {
-                return false;
-            } else if (year%100 != 0) {
-                return false;
-            } else if (year%400 != 0) {
-                return false;
-            } else return true;
-        }
+        if (year%4 != 0)
+            return false;
+        else if (year%100 != 0)
+            return true;
+        else if (year%400 != 0)
+            return false;
+        else return true;
     }
+}
 
 
 
