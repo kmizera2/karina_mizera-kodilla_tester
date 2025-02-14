c07f94eb9019673bcc41e18145240a3435ae3c94
kmizera <karina.mizera@gmail.com>
2025-02-14 15:14:14 +0100

    reformat code LeapYear

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,14 +1,10 @@
-import java.util.Arrays;
-
 public class LeapYear {
-    public static boolean leapYear (int year){
-        if (year%4 != 0)
+    public static boolean leapYear(int year) {
+        if (year % 4 != 0)
             return false;
-        else if (year%100 != 0)
+        else if (year % 100 != 0)
             return true;
-        else if (year%400 != 0)
-            return false;
-        else return true;
+        else return year % 400 == 0;
     }
 }
 
