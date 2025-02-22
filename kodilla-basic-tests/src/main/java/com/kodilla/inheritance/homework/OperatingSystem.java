73acc1bbe42749448e6431834e0073894a6be253
kmizera <karina.mizera@gmail.com>
2025-02-22 15:53:50 +0100

    OperatingSystem

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
@@ -1,4 +1,19 @@
 package com.kodilla.inheritance.homework;
 
 public class OperatingSystem {
+
+    private int releaseYear;
+
+    public OperatingSystem (int releaseYear){
+        this.releaseYear = releaseYear;
+    }
+
+    public void turnOn(){
+        System.out.println("System was turn on");
+    }
+
+    public void turnOff(){
+        System.out.println("System was turn off");
+    }
+
 }
