d578a5dd1b1dcbbe2fc5e4a3055de30068e49352
kmizera <karina.mizera@gmail.com>
2025-02-22 15:58:48 +0100

    Subsystem2

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Subsystem2.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Subsystem2.java
@@ -1,4 +1,18 @@
 package com.kodilla.inheritance.homework;
 
-public class Subsystem2 {
+public class Subsystem2 extends OperatingSystem{
+
+    public Subsystem2(int releaseYear) {
+        super(releaseYear);
+    }
+
+    @Override
+    public void turnOn(){
+        System.out.println("Subystem2 was turn on");
+    }
+
+    @Override
+    public void turnOff(){
+        System.out.println("Subystem2 was turn off");
+    }
 }
