6f0415248e67164bcef3e0084890ccb8ff5aa2ac
kmizera <karina.mizera@gmail.com>
2025-02-23 15:59:49 +0100

    First Class Clean code

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,21 +1,19 @@
 public class FirstClass {
     public static void main(String[] args) {         // [2]
         Notebook notebook = new Notebook(600, 1000, 1999);
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2020);
+        Notebook oldNotebook = new Notebook(1600, 0, 2024);
+
+        displayAllNotebookParameters(notebook);
+        displayAllNotebookParameters(heavyNotebook);
+        displayAllNotebookParameters(oldNotebook);
+    }
+
+    private static void displayAllNotebookParameters(Notebook notebook) {
         System.out.println(notebook.weight + " " + notebook.price);
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkYearAndPrice();
-
-        Notebook heavyNotebook = new Notebook(2000, 1500, 2020);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
-        heavyNotebook.checkPrice();
-        heavyNotebook.checkWeight();
-        heavyNotebook.checkYearAndPrice();
-
-        Notebook oldNotebook = new Notebook(1600, 0, 2024);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
-        oldNotebook.checkPrice();
-        oldNotebook.checkWeight();
-        oldNotebook.checkYearAndPrice();
     }
+
 }
