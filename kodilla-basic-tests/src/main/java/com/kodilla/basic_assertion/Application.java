756e6dad631f1cd89b3c6a2d68ce4d0bbed752e0
kmizera <karina.mizera@gmail.com>
2025-02-23 16:11:16 +0100

    Calculator with using Math library

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -15,20 +15,12 @@ public class Application {
             System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
         }
 
-        int substractResult = calculator.subtract(a, b);
-        boolean correct1 = ResultChecker.assertEquals(-3, substractResult);
+        int subtractResult = calculator.subtract(a, b);
+        boolean correct1 = ResultChecker.assertEquals(-3, subtractResult);
         if (correct1) {
             System.out.println("Metoda substract działa poprawnie dla liczb " + a + " i " + b);
         } else {
             System.out.println("Metoda substract nie działa poprawnie dla liczb " + a + " i " + b);
-        }
-
-        int squareResult = calculator.square(a);
-        boolean correct3 = ResultChecker.assertEquals(25, squareResult);
-        if (correct3) {
-            System.out.println("Metoda square działa poprawnie dla liczby " + a);
-        } else {
-            System.out.println("Metoda square nie działa poprawnie dla liczby " + a);
         }
 
         double squareDoubleResult = calculator.squareDouble(c);
--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -3,23 +3,11 @@ package com.kodilla.basic_assertion;
 public class Calculator {
 
     public int sum(int a, int b) {
-        return a + b;
+        return Math.addExact(a, b);
     }
 
     public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public int square(int a) {
-        return a * a;
-    }
-
-    public double sumDouble(double a, double b) {
-        return a + b;
-    }
-
-    public double subtractDouble(double a, double b) {
-        return a - b;
+        return Math.subtractExact(a, b);
     }
 
     public double squareDouble(double a) {
