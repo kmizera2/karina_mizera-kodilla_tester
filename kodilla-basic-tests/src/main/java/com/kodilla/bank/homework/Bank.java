99e87c19c690d1270e8c14d957ff335782f38146
kmizera <karina.mizera@gmail.com>
2025-03-02 02:29:27 +0100

    add bank homework

--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -1,4 +1,65 @@
 package com.kodilla.bank.homework;
 
 public class Bank {
+
+    public CashMachine[] cashMachines;
+
+    public Bank(int size) {
+        this.cashMachines = new CashMachine[size];
+    }
+
+    public double getTotalBalance() {
+        double totalBalance = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            totalBalance += cashMachines[i].checkBalance();
+        }
+        return totalBalance;
+    }
+
+
+    public int getDepositsCount() {
+        int depositsCounter = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            for (int j = 0; j < cashMachines[i].transactions.length; j++) {
+                if (cashMachines[i].transactions[j] > 0)
+                    depositsCounter++;
+            }
+        }
+        return depositsCounter;
+    }
+
+    public int getWithdrawalCount() {
+        int withdrawalCounter = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            for (int j = 0; j < cashMachines[i].transactions.length; j++) {
+                if (cashMachines[i].transactions[j] < 0)
+                    withdrawalCounter++;
+            }
+        }
+        return withdrawalCounter;
+    }
+
+
+    public double getDepositsAverage() {
+        double totalDeposits = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            for (int j = 0; j < cashMachines[i].transactions.length; j++) {
+                if (cashMachines[i].transactions[j] > 0)
+                    totalDeposits += cashMachines[i].transactions[j];
+            }
+        }
+        return totalDeposits / getDepositsCount();
+    }
+
+    public double getWithdrawalAverage() {
+        double totalWithdrawal = 0;
+        for (int i = 0; i < cashMachines.length; i++) {
+            for (int j = 0; j < cashMachines[i].transactions.length; j++) {
+                if (cashMachines[i].transactions[j] < 0)
+                    totalWithdrawal += cashMachines[i].transactions[j];
+            }
+        }
+        return totalWithdrawal / getWithdrawalCount();
+    }
 }
+
--- kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -1,4 +1,36 @@
 package com.kodilla.bank.homework;
 
 public class CashMachine {
+
+    public double[] transactions;
+    private int size;
+    private double balance =0;
+
+    public CashMachine() {
+        this.size = 0;
+        this.transactions = new double[0];
+    }
+
+    public void addTransaction(int value) {
+        if (value !=0) {
+            this.size++;
+            double[] newTab = new double[this.size];
+            System.arraycopy(transactions, 0, newTab, 0, transactions.length);
+            newTab[this.size - 1] = value;
+            this.transactions = newTab;
+        }
+        else System.out.println("Wykonanie operacji na wybraną kwotę jest niemożliwe");
+    }
+
+
+    public double checkBalance() {
+        for (int i = 0; i < transactions.length; i++) {
+            balance += transactions[i];
+        }
+        return balance;
+    }
+
+    public int countTransactions (){
+        return transactions.length;
+    }
 }
