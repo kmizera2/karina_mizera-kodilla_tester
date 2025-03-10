8ca9e0489730a90d979c782157ecb572266faa54
kmizera <karina.mizera@gmail.com>
2025-03-10 01:09:32 +0100

    flightFinderTestSuite

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
@@ -1,4 +1,42 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-class FlightFinderTest {
-  
+
+class FlightFinderTestSuite {
+
+
+    @Test
+    void testFindFlightsFrom() {
+        FlightFinder finder = new FlightFinder();
+        List<Flight> flightsFromWarsaw = finder.findFlightsFrom("Warsaw");
+        assertEquals(2, flightsFromWarsaw.size());
+    }
+
+    @Test
+    void testFindFlightsTo() {
+        FlightFinder finder = new FlightFinder();
+        List<Flight> flightsToWarsaw = finder.findFlightsTo("Warsaw");
+        assertEquals(3, flightsToWarsaw.size());
+    }
+
+    @Test
+    void testZeroFlightsTo() {
+        FlightFinder finder = new FlightFinder();
+        List<Flight> flightsToPeczniew = finder.findFlightsTo("Pęczniew");
+        assertEquals(0, flightsToPeczniew.size());
+    }
+
+    @Test
+    void testZeroFlightsFrom() {
+        FlightFinder finder = new FlightFinder();
+        List<Flight> flightsFromPeczniew = finder.findFlightsFrom("Pęczniew");
+        assertEquals(0, flightsFromPeczniew.size());
+    }
+
+
 }
