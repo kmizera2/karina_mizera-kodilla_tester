4d0c243f80ce62467cfff096fe54b511cbee2fac
kmizera <karina.mizera@gmail.com>
2025-04-20 20:37:38 +0200

    numbers.csv

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/InvalidNumbersException.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/InvalidNumbersException.java
@@ -1,4 +1,7 @@
 package com.kodilla.parametrized_tests.homework;
 
-public class InvalidNumbersException {
+public class InvalidNumbersException extends Exception{
+    public InvalidNumbersException() {
+        super("rong numbers provided");
+    }
 }
