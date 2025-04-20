38bd6660f4be189b5e395f46a1a7e027b152cd85
kmizera <karina.mizera@gmail.com>
2025-04-21 00:47:39 +0200

    PersonTest (BMI)

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTest.java
@@ -1,4 +1,18 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonTest {
-  
+    @ParameterizedTest
+    @MethodSource("com.kodilla.parametrized_tests.homework.BMIDataProvider#bmiData")
+    void shouldReturnCorrectBmiCategory(double height, double weight, String expectedCategory) {
+        Person person = new Person(height, weight);
+        assertEquals(expectedCategory, person.getBMI(),
+                () -> String.format("Expected BMI category '%s' for height=%.2f, weight=%.2f but was '%s'",
+                        expectedCategory, height, weight, person.getBMI()));
+    }
+
 }
