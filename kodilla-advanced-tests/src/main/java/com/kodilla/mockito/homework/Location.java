5a3e93f9ca64b136c1ef401448d567f3a678444a
kmizera <karina.mizera@gmail.com>
2025-04-22 16:58:39 +0200

    Add location for weather ntifications

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/Location.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/Location.java
@@ -1,4 +1,26 @@
 package com.kodilla.mockito.homework;
 
-public interface Location {
+public class Location {
+    private final String name;
+
+    public Location(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return name.equals(location.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
