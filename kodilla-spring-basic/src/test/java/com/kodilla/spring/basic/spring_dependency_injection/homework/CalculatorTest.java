d7fc55300dfdb28a846eb368a33141a0ce750644
kmizera <karina.mizera@gmail.com>
2025-04-28 03:49:57 +0200

    spring_dependency_injection/homework/CalculatorTest

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTest.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTest.java
@@ -1,4 +1,45 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+
 class CalculatorTest {
-  
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void shouldAddNumbers() {
+        double result = calculator.add(2, 3);
+        assertEquals(5, result, 0.01);
+    }
+
+    @Test
+    void shouldSubtractNumbers() {
+        double result = calculator.subtract(10, 7);
+        assertEquals(3, result, 0.01);
+    }
+
+    @Test
+    void shouldMultiplyNumbers() {
+        double result = calculator.multiply(4, 5);
+        assertEquals(20, result, 0.01);
+    }
+
+    @Test
+    void shouldDivideNumbers() {
+        double result = calculator.divide(12, 3);
+        assertEquals(4, result, 0.01);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenDividingByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(10, 0));
+        assertEquals("Nie można dzielić przez zero.", exception.getMessage());
+    }
 }
