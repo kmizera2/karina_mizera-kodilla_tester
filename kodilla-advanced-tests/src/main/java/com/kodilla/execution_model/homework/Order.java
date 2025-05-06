137b22a34cb0f3ac6dd8d4cccddd74470611c474
kmizera <karina.mizera@gmail.com>
2025-05-06 13:29:20 +0200

    execution_model/homework/Order & shop

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
@@ -1,4 +1,44 @@
 package com.kodilla.execution_model.homework;
 
+import java.util.Objects;
+import java.time.LocalDate;
+
+
 public class Order {
+    private double orderValue;
+    private LocalDate orderDate;
+    private String userLogin;
+
+    public Order(double orderValue, LocalDate orderDate, String userLogin) {
+        this.orderValue = orderValue;
+        this.orderDate = orderDate;
+        this.userLogin = userLogin;
+    }
+
+    public double getOrderValue() {
+        return orderValue;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Double.compare(order.orderValue, orderValue) == 0 &&
+                Objects.equals(orderDate, order.orderDate) &&
+                Objects.equals(userLogin, order.userLogin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderValue, orderDate, userLogin);
+    }
 }
--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -1,4 +1,38 @@
 package com.kodilla.execution_model.homework;
+import java.util.HashSet;
 
-public class Shop {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Shop {
+
+    private Set<Order> orders = new HashSet<>();
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public List<Order> getOrdersBetweenDates(LocalDate from, LocalDate to) {
+        return orders.stream()
+                .filter(order -> !order.getOrderDate().isBefore(from) && !order.getOrderDate().isAfter(to))
+                .collect(Collectors.toList());
+    }
+
+    public List<Order> getOrdersByValueRange(double minValue, double maxValue) {
+        return orders.stream()
+                .filter(order -> order.getOrderValue() >= minValue && order.getOrderValue() <= maxValue)
+                .collect(Collectors.toList());
+    }
+
+    public int getNumberOfOrders() {
+        return orders.size();
+    }
+
+    public double getTotalOrderValue() {
+        return orders.stream()
+                .mapToDouble(Order::getOrderValue)
+                .sum();
+    }
 }
