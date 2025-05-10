76f61a7226d00cf2a451e5e0d4a6dafbd4a0d085
kmizera <karina.mizera@gmail.com>
2025-05-10 23:19:59 +0200

    notificationServie

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/NotificationService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/NotificationService.java
@@ -1,4 +1,14 @@
 package com.kodilla.spring.basic.spring_dependency_injection.homework;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationService {
+    public String success(String address) {
+        return "Package delivered to: " + address;
+    }
+
+    public String fail(String address) {
+        return "Package not delivered to: " + address;
+    }
 }
