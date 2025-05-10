d65b0630eb325a88d18264821cae55be4f66f1c0
kmizera <karina.mizera@gmail.com>
2025-05-11 01:17:35 +0200

    clock

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -1,4 +1,23 @@
 package com.kodilla.spring.basic.spring_scopes.homework;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+@Scope("prototype")
 public class Clock {
+
+    private final LocalTime time;
+
+    public Clock() {
+        this.time = LocalTime.now();
+        System.out.println("Clock bean created at: " + time);
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
 }
+
