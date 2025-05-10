8458d4e3cbbe972758084a56b05e4fd686b02d5c
kmizera <karina.mizera@gmail.com>
2025-05-10 23:20:40 +0200

    deliveryService

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/DeliveryService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/DeliveryService.java
@@ -1,4 +1,14 @@
 package com.kodilla.spring.basic.spring_dependency_injection.homework;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class DeliveryService {
+    public boolean deliverPackage(String address, double weight) {
+        if (weight > 30) {
+            return false;
+        }
+        return true;
+    }
 }
