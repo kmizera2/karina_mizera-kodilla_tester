0277453ecdb12f6ac77dbe1b0c6cfe8389f41d78
kmizera <karina.mizera@gmail.com>
2025-05-12 04:20:06 +0200

    changing returning type of method Add book

--- kodilla-rest-api/src/main/java/com/kodilla/rest/controller/BookController.java
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/controller/BookController.java
@@ -22,8 +22,9 @@ class BookController {
     }
 
     @PostMapping
-    public void addBook(@RequestBody BookDto bookDto) {
+    public BookDto addBook(@RequestBody BookDto bookDto) {
         bookService.addBook(bookDto);
+        return bookDto;
     }
 
     @DeleteMapping
