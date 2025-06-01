3cc9e848e88b753b349681a577dd27878ed554b1
kmizera <karina.mizera@gmail.com>
2025-06-02 01:34:29 +0200

    container tests - movie and screenshots

---
+++ kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/businessCardTest.java
@@ -0,0 +1,64 @@
+package com.kodilla.testcontainers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.DefaultRecordingFileFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.SKIP;
+
+public class businessCardTest {
+    @Rule
+    public Network network = Network.newNetwork();
+
+    @Rule
+    public GenericContainer webServer =
+            new GenericContainer(
+                    new ImageFromDockerfile()
+                            .withFileFromPath("/tmp/index.html", Paths.get("C:\\docker-wizytowka\\index.html") )
+                            .withDockerfileFromBuilder(builder ->
+                                    builder
+                                            .from("httpd:2.4")
+                                            .copy("/tmp/index.html", "/usr/local/apache2/htdocs/index.html")
+                                            .build()))
+                    .withNetwork(network)
+                    .withNetworkAliases("my-server")
+                    .withExposedPorts(80);
+
+    @Rule
+    public BrowserWebDriverContainer chrome =
+            (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+                    .withCapabilities(new ChromeOptions())
+                    .withRecordingMode(RECORD_ALL, new File("./build/"))
+                    .withRecordingFileFactory(new DefaultRecordingFileFactory())
+                    .withNetwork(network);
+
+    @Test
+    public void customImageTest() throws InterruptedException, IOException {
+        RemoteWebDriver driver = chrome.getWebDriver();
+        driver.get("http://my-server/");
+
+        File screenshot = driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File("./build/screenshots/" + screenshot.getName()));
+
+        String header = driver.findElement(By.xpath("//h1")).getText();
+        assertEquals("Karina Mizera", header);
+    }
+}
